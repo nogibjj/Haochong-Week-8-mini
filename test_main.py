4381fc79c17c9a8e8c3d180bbc0e362971e24cf6
hxia5 <hxia5@illinois.edu>
2023-10-20 18:10:37 -0400

    aa

--- test_main.py
+++ test_main.py
@@ -1,6 +1,3 @@
-from main import (
-    get_median
-)
 import pandas as pd
 
 example_csv = "https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv"
